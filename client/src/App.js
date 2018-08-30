438001a765a491269449bef1d969ae5e26394ac3
Joshua Langer <joshuarllanger@gmail.com>
2018-08-30 14:47:07 -0500

    added React frontend

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Heidy ho!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
