08e128862cefbf31d12255459e6c4962906b5278
Joshua Langer <joshuarllanger@gmail.com>
2018-09-01 14:27:02 -0500

    hooked up addItem and deleteItem actions

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,5 +1,5 @@
 import uuid from 'uuid';
-import {GET_ITEMS, ADD_ITEM, DELETE_ITEM} from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
 
 const initialState = {
   items: [
@@ -24,6 +24,18 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case GET_ITEMS:
       return { ...state };
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: [action.payload, ...state.items]
+      };
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter(item => {
+          return item.id !== action.payload;
+        })
+      };
 
     default:
       return state;
